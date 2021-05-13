7c74f6104461b47df87b2c43e6a7a015d74997f5
Beren Minor <beren.minor+git@gmail.com>
2021-05-13 13:23:26 +0200

    Ignore non-package lines when reading Pipfile.
    Closes: #2

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 requires = [r.replace(' = "*"','').strip() for r in open('Pipfile').readlines()
-            if '[packages]' not in r]
+            if '[' not in r and len(r.strip()) > 0]
 
 setup(name='steam-cli', url='https://github.com/berenm/steam-cli',
 	license='UNLICENSE',
